a17397be6c9bb7908b6676a813005dbb8973515a
Krish-006-19 <krish.ramena24@vit.edu>
2025-04-28 12:15:02 +0530

    fcomit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React, { useEffect } from 'react'
 import SideBar from './features/SideBar'
 import Feed from './features/Feed'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import SignIn from './features/SignIn'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
+import { login, logout } from './Redux/userslice'
 
 function App() {
   const user = useSelector(state => state.user.user)
+  const dispatch = useDispatch()
   useEffect(()=>{
     onAuthStateChanged(auth,(user)=>{
-      
+      if(user)
+        dispatch(login({
+          name: user.displayName,
+          imgurl: user.photoURL,
+          email: user.email
+        }))
+      else{
+        dispatch(logout())
+      }
     })
   },[])
   return (
