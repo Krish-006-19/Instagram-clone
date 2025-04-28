7cb5588c3c915a8ec5d9b34f6a66e781f66f01e3
Krish-006-19 <krish.ramena24@vit.edu>
2025-04-28 14:04:12 +0530

    latest

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Routes,Route, Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App'
 import SignUp from './features/SignUp'
 
 function App1() {
   return (
-        <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Navigate to='/Insta-clone'/>}/>
-        <Route path='/Insta-clone' element={<App/>}/>
-        <Route path='/Insta-clone/' element={<App/>}/>
-        <Route path='/Insta-clone/signup' element={<SignUp/>}/>
+        <Route path="/" element={<Navigate to="/Insta-clone" />} />
+        <Route path="/Insta-clone" element={<App />} />
+        <Route path="/Insta-clone/signup" element={<SignUp />} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   )
 }
 
